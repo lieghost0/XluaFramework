b69b9a102c09e1d8edaa4292c0eb1ac0e10e4264
luz <1678113401@qq.com>
2025-07-25 08:39:56 +0800

    add first buildtool

---
+++ Assets/Scrips/Editor/BuildTool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildTool : Editor
+{
+    [MenuItem("Tools/Build Windows Bundle")]
+    static void BundleWindowsBuild()
+    {
+        Build(BuildTarget.StandaloneWindows, "Windows打包完成");
+    }
+    [MenuItem("Tools/Build Android Bundle")]
+    static void BundleAndroidBuild()
+    {
+        Build(BuildTarget.Android, "Android打包完成");
+    }
+    [MenuItem("Tools/Build IPhone Bundle")]
+    static void BundleIPhoneBuild()
+    {
+        Build(BuildTarget.iOS, "IOS打包完成");
+    }
+    static void Build(BuildTarget target, string msg)
+    {
+        List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+        string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories);
+        List<string> logs = new List<string>();
+        for(int i = 0; i < files.Length; i++)
+        {
+            if (files[i].EndsWith(".meta"))
+                continue;
+            AssetBundleBuild assetBundle = new AssetBundleBuild();
+
+            string fileName = PathUtil.GetStandardPath(files[i]);
+            logs.Add(fileName);
+
+            string assetName = PathUtil.GetUnityPath(fileName);
+            assetBundle.assetNames = new string[] { assetName };
+            string bundleName = files[i].Replace(PathUtil.BuildResourcesPath, ".").ToLower();
+            assetBundle.assetBundleName = bundleName + ".ab";
+            assetBundleBuilds.Add(assetBundle);
+        }
+
+        if(Directory.Exists(PathUtil.BundleOutPath))
+            Directory.Delete(PathUtil.BundleOutPath, true);
+        Directory.CreateDirectory(PathUtil.BundleOutPath);
+
+
+        BuildPipeline.BuildAssetBundles(PathUtil.BundleOutPath, assetBundleBuilds.ToArray(), BuildAssetBundleOptions.None, target);
+        foreach(var log in logs)
+        {
+            Debug.Log(log);
+        }
+        EditorUtility.DisplayDialog("提示", msg, "确定");
+    }
+}
